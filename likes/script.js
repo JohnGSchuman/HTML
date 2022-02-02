91c09aa894cf091ae1f20486916cc0795753c852
John Schuman <MusicParkRidge@gmail.com>
2022-02-01 22:29:39 -0600

    initial commit

---
+++ likes/script.js
@@ -0,0 +1,12 @@
+function likeAdd1() {
+    var likeNew = document.querySelector(".like1").
+    innerText++;
+}
+function likeAdd2() {
+    var likeNew = document.querySelector(".like2").
+    innerText++;
+}
+function likeAdd3() {
+    var likeNew = document.querySelector(".like3").
+    innerText++;
+}
